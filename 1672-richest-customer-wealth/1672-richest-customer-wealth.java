5c378ccefeab7e812b8dd1c2c30c3e45c64f7f48
Sabhyata Verma <sabhyata7405@gmail.com>
2026-03-02 17:55:35 +0530

    Time: 0 ms (100%), Space: 44.7 MB (55.71%) - LeetHub

--- 1672-richest-customer-wealth/1672-richest-customer-wealth.java
+++ 1672-richest-customer-wealth/1672-richest-customer-wealth.java
@@ -3,12 +3,13 @@ class Solution {
         int max=Integer.MIN_VALUE;
         int row=accounts.length;
         int col=accounts[0].length;
+        int sum=0;
         for(int i=0; i<row;i++){
-            int sum=0;
             for(int j=0; j<col; j++){
                 sum+=accounts[i][j];
-                max=Math.max(max,sum);
             }
+            max=Math.max(max,sum);
+            sum=0;
         }
         return max;
     }
